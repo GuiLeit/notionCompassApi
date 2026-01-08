19a5f4278eebd1fd55a667e16bb311c31dc989cf
GuiLeit <guiadse@gmail.com>
2026-01-07 21:26:44 -0300

    init

---
+++ src/test/java/com/guilhermeleite/NotionCompass/NotionCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeleite.NotionCompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
