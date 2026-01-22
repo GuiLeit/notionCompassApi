b4b9c0768fc0b76efeda372faa5d84ed05db5179
GuiLeit <guiadse@gmail.com>
2026-01-21 22:25:22 -0300

    feat: cors validation implemented

--- src/main/java/com/guilhermeleite/NotionCompass/security/SecurityConfigurations.java
+++ src/main/java/com/guilhermeleite/NotionCompass/security/SecurityConfigurations.java
@@ -1,10 +1,12 @@
 package com.guilhermeleite.NotionCompass.security;
 
+import com.guilhermeleite.NotionCompass.config.NotionProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,26 +15,48 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
     @Autowired
-    SecurityFilter SecurityFilter;
+    SecurityFilter securityFilter;
+
+    @Autowired
+    NotionProperties notionProperties;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/", "/webhook/secret", "/notion/auth/login", "/notion/auth/callback").permitAll()
                         .requestMatchers(HttpMethod.POST, "/webhook").permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(SecurityFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(this.securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("chrome-extension://" + this.notionProperties.getExtentionId()));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply this configuration to all paths
+        return source;
     }
 
     @Bean
